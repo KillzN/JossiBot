ab84eef36afdc7a2779278a7a7025bb97b7fc4ff
KillzN <scorpionsheep203@gmail.com>
2024-08-18 20:35:49 -0400

    KILL - BOT

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,5 +1,5 @@
 const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  const datas = globalw
+  const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.gc_tagall
@@ -10,12 +10,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `* 𝗔𝗖𝗧𝗜𝗩𝗘𝗡𝗦𝗘:* ${pesan}`;
+  const oi = `*𝗔𝗖𝗧𝗜𝗩𝗘𝗡𝗦𝗘:* ${pesan}`;
   let teks = `*> 𝘽𝙚𝙘𝙖𝙪𝙨𝙚 𝙩𝙤𝙢𝙤𝙧𝙧𝙤𝙬 𝙬𝙞𝙡𝙡 𝙗𝙚 𝙣𝙞𝙘𝙚 💗*\n\n ${oi}\n\n➥ _*LISTA DE INTEGRANTES:*_\n`;
   for (const mem of participants) {
-    teks += `💸  ⇝ @${mem.id.split('@')[0]}\n`;
+    teks += `💸 ⇝ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└ *JossiBᴏᴛ ⇝ @by.jossi*`;
+  teks += `└ *JossiBᴏᴛ ⇝@by.jossi*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
